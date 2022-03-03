12af571f97bb026a9bbefd8924669f296f884f8d
Daniel Muñoz <dsantiagomj@gmail.com>
2022-03-03 13:09:47 -0500

    create basic counter component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { useSelector } from 'react-redux';
 function App() {
-  return <div className="App"></div>;
+  const counter = useSelector((state) => state.counter);
+  const increment = () => {};
+  const decrement = () => {};
+
+  return (
+    <div className="App">
+      <h1>Counter</h1>
+      <p>{counter}</p>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
+    </div>
+  );
 }
 
 export default App;
